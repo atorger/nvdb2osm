ecca0db4119e32eb1e7a5c55102f0e6f948315d3
Anders Torger <anders@torger.se>
2022-10-29 20:54:08 +0200

    lint fix

--- proj_xy.py
+++ proj_xy.py
@@ -4,4 +4,4 @@ sweref99_transformer = Transformer.from_crs("epsg:3006", "epsg:4326")
 
 def latlon_str(xy):
     lat, lon = sweref99_transformer.transform(xy[1], xy[0])
-    return "<lat:%s lon:%s>" % (lat, lon)
+    return f"<lat:{lat} lon:{lon}>"
